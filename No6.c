163a4b3409b5ac584bf0f9743323d13ee3e9d2c9
ha213176 <61187538+ha213176@users.noreply.github.com>
2020-06-18 14:24:42 +0800

    Create No6.c

---
+++ No6.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main(){
+    char buf[10][100];
+    int idx = 0, k = 0;
+    int len;
+    FILE *fp = fopen("data", "r+");
+    while(fgets(buf[idx], 100, fp) != NULL){
+        len = strlen(buf[idx]);
+        for(int i = 0; i < len; i++){
+            if(buf[idx][i] >= 'A' && buf[idx][i] <= 'Z'){
+                buf[idx][i] += ('a' - 'A');
+            }
+            if(buf[idx][i] != '\n' && buf[idx][i] != '\0'){
+                k = 1;
+            }
+        }
+        //fputs(buf[idx], fp);
+        if(k == 1){
+            idx++;
+            k = 0;
+        }
+    }
+    fclose(fp);
+    fp = fopen("data", "r+");
+    for(int i = 0; i < idx; i++){
+        fprintf(fp, "%s", buf[i]);
+    }
+    fclose(fp);
+    return 0;
+}
